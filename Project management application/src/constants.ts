d18e7ebd6b599c21819ca25cf28e5531ca752f7d
Mitya <partyzan327@gmail.com>
2022-11-09 15:50:26 +0300

    refactor: var

--- Project management application/src/constants.ts
+++ Project management application/src/constants.ts
@@ -1,2 +1,3 @@
 export const PORT = 3000;
 export const SECRET_KEY = 'myVerySecretKey-04.05.2022';
+export const MONGO_CONNECTION_STRING = 'mongodb+srv://pma:pma@cluster0.drvaldg.mongodb.net/?retryWrites=true&w=majority'
--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
-import { PORT } from './constants';
+import { MONGO_CONNECTION_STRING, PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://pma:pma@cluster0.drvaldg.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect(MONGO_CONNECTION_STRING);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
